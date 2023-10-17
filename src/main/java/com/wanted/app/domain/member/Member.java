414c49adcb16ed8e5267cf1c06e995012cf0f066
min050410 <min050410@gmail.com>
2023-10-18 01:13:39 +0900

    feat(Entity): 멤버 entity 정의

---
+++ src/main/java/com/wanted/app/domain/member/Member.java
@@ -0,0 +1,34 @@
+package com.wanted.app.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Builder
+    private Member(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+}
