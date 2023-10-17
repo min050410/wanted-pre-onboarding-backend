c08d541c1e584392517a7a055ec81628bfb10e4f
min050410 <min050410@gmail.com>
2023-10-18 01:14:02 +0900

    feat(Entity): 지원내역 entity 정의

---
+++ src/main/java/com/wanted/app/domain/application/Application.java
@@ -0,0 +1,40 @@
+package com.wanted.app.domain.application;
+
+import com.wanted.app.domain.company.Company;
+import com.wanted.app.domain.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Builder
+    private Application(Company company, Member member) {
+        this.company = company;
+        this.member = member;
+    }
+
+}
