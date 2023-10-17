2fd803def8c79852f51e304ea5c47b76de2dba4e
min050410 <min050410@gmail.com>
2023-10-18 01:19:59 +0900

    feat(ApiPath): ApiPath 설정

---
+++ src/main/java/com/wanted/app/web/path/ApiPath.java
@@ -0,0 +1,16 @@
+package com.wanted.app.web.path;
+
+public class ApiPath {
+
+    // 채용 공고
+    public static final String JOB_OPENING_CREATE = "/api/v1/opening";
+    public static final String JOB_OPENING_DELETE = "/api/v1/opening/delete/{id}";
+    public static final String JOB_OPENING_UPDATE = "/api/v1/opening/update/{id}";
+    public static final String JOB_OPENING_VIEW_FILTER = "/api/v1/opening/filter";
+    public static final String JOB_OPENING_VIEW_DETAIL = "/api/v1/opening/detail/{id}";
+
+
+    // 지원
+    public static final String JOB_APPLY = "/api/v1/apply";
+
+}
